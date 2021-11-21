18144307a89b4ca5c3d5e1205eddc9e97ec00392
ali ihsan erdem <ali@ihsanerdem.com>
2021-11-21 16:07:07 +0300

    added requests.get

--- wizardapp/management/commands/dojob.py
+++ wizardapp/management/commands/dojob.py
@@ -1,7 +1,8 @@
 from django.core.management.base import BaseCommand, CommandError
-
+import requests
 
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        requests.get('https://happy-engelbart.35-83-202-179.plesk.page/kek12.web')
         print('command executed')
